bd76d6529a468179ed6ded030af7cc449de8f818
Edward Cross <edward.k.cross@gmail.com>
2018-07-31 15:17:57 +0200

    ParanoidLauncher: Move callbacks to outer class
    Callbacks class will handle alot of our changes, move it to it's own outer class for better management
    
    Change-Id: I28cde0b69212e8322380fdc41e9d4f2d758faf8e

--- src/com/paranoid/launcher/ParanoidLauncher.java
+++ src/com/paranoid/launcher/ParanoidLauncher.java
@@ -15,94 +15,11 @@
  */
 package com.paranoid.launcher;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.os.Bundle;
-
-import com.android.launcher3.AppInfo;
 import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherCallbacks;
-
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class ParanoidLauncher extends Launcher {
 
     public ParanoidLauncher() {
         setLauncherCallbacks(new ParanoidLauncherCallbacks(this));
-    }
-
-    public class ParanoidLauncherCallbacks implements LauncherCallbacks, OnSharedPreferenceChangeListener {
-
-        private final ParanoidLauncher mLauncher;
-
-        public ParanoidLauncherCallbacks(ParanoidLauncher launcher) {
-            mLauncher = launcher;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) { }
-
-        @Override
-        public void onResume() { }
-
-        @Override
-        public void onStart() { }
-
-        @Override
-        public void onStop() { }
-
-        @Override
-        public void onPause() { }
-
-        @Override
-        public void onDestroy() { }
-
-        @Override
-        public void onSaveInstanceState(Bundle outState) { }
-
-        @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent data) { }
-
-        @Override
-        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) { }
-
-        @Override
-        public void onAttachedToWindow() { }
-
-        @Override
-        public void onDetachedFromWindow() { }
-
-        @Override
-        public void dump(String prefix, FileDescriptor fd, PrintWriter w, String[] args) { }
-
-        @Override
-        public void onHomeIntent(boolean internalStateHandled) { }
-
-        @Override
-        public boolean handleBackPressed() {
-            return false;
-        }
-
-        @Override
-        public void onTrimMemory(int level) { }
-
-        @Override
-        public void onStateChanged() { }
-
-        @Override
-        public void onLauncherProviderChange() { }
-
-        @Override
-        public boolean startSearch(String initialQuery, boolean selectInitialQuery, Bundle appSearchData) {
-            return false;
-        }
-
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
-
     }
 }
