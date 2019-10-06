be5221b6308a31bcd64ede307a9674de279f113e
Chris Crump <chris@cypheros.co>
2019-10-06 18:38:24 -0400

    ParanoidLauncher: Create custom settings activity
    Extends the current Launcher3 settings activity. Here we will include our additions
    
    Change-Id: Ie488a7ac6503011fd3235e7a99e1db1848cb516f

---
+++ src/com/paranoid/launcher/settings/ParanoidSettingsActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paranoid.launcher;
+
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+
+import com.android.launcher3.settings.SettingsActivity;
+import com.android.launcher3.settings.SettingsActivity.LauncherSettingsFragment;
+
+public class ParanoidSettingsActivity extends SettingsActivity {
+
+    /**
+     * This fragment shows the Paranoid launcher preferences.
+     */
+    public static class ParanoidSettingsFragment extends LauncherSettingsFragment {
+
+        /**
+         * Initializes a preference. This is called for every preference. Returning false here
+         * will remove that preference from the list.
+         */
+		@Override
+        protected boolean initPreference(Preference preference) {
+            super.initPreference(preference);
+			return true;
+        }
+    }
+}
