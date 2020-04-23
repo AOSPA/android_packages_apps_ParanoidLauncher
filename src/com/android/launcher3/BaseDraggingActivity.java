24cc5a7951345a57662d83326934bb626dc5794b
Chris Crump <chris@cypheros.co>
2020-04-23 02:37:16 -0400

    ParanoidLauncher: Add support for quick task access
    Change-Id: I52c1ab9b5e95aaaad361fd9af5910d9faa16d46f

--- src/com/android/launcher3/BaseDraggingActivity.java
+++ src/com/android/launcher3/BaseDraggingActivity.java
@@ -41,6 +41,8 @@ import com.android.launcher3.uioverrides.WallpaperColorInfo;
 import com.android.launcher3.util.PackageManagerHelper;
 import com.android.launcher3.util.Themes;
 
+import com.paranoid.quickstep.views.TaskIconsView;
+
 /**
  * Extension of BaseActivity allowing support for drag-n-drop
  */
@@ -119,6 +121,8 @@ public abstract class BaseDraggingActivity extends BaseActivity
     public abstract <T extends View> T getOverviewPanel();
 
     public abstract View getRootView();
+
+    public abstract TaskIconsView getTaskIconsView();
 
     public void returnToHomescreen() {
         // no-op
