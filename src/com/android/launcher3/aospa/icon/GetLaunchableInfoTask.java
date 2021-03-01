LineageOS/android_packages_apps_Trebuchet

Launcher3: Add support for icon packs
- Supports icon packs from nova and adw
- Live preview in settings
- Default applied icon pack via overlay

Change-Id: Ie9879fa30f4cd9d52356acf78a423b415657510c
Signed-off-by: Joey <joey@lineageos.org>

src/com/android/launcher3/lineage/icon/GetLaunchableInfoTask.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.lineage.icon;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.LauncherActivityInfo;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Process;
+import android.os.UserHandle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+final class GetLaunchableInfoTask extends AsyncTask<Void, Void, List<LauncherActivityInfo>> {
+
+    private final PackageManager pm;
+    private final LauncherApps launcherApps;
+    private final int limit;
+    private final Callback callback;
+
+    GetLaunchableInfoTask(PackageManager pm,
+                          LauncherApps launcherApps,
+                          int limit,
+                          Callback callback) {
+        this.pm = pm;
+        this.launcherApps = launcherApps;
+        this.limit = limit;
+        this.callback = callback;
+    }
+
+    @Override
+    protected List<LauncherActivityInfo> doInBackground(Void... voids) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            // This should never happen
+            return new ArrayList<>();
+        }
+
+        final UserHandle ua = Process.myUserHandle();
+        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null)
+                .addCategory(Intent.CATEGORY_LAUNCHER);
+        return pm.queryIntentActivities(mainIntent, 0)
+            .parallelStream()
+            .sorted(new ResolveInfo.DisplayNameComparator(pm))
+            .limit(limit)
+            .map((ri) -> {
+                final ActivityInfo ai = ri.activityInfo;
+                final Intent i = new Intent();
+                i.setClassName(ai.applicationInfo.packageName, ai.name);
+                return launcherApps.resolveActivity(i, ua);
+            })
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void onPostExecute(List<LauncherActivityInfo> list) {
+        callback.onLoadCompleted(list);
+    }
+
+    interface Callback {
+        void onLoadCompleted(List<LauncherActivityInfo> result);
+    }
+}

src/com/android/launcher3/lineage/icon/IconPackHeaderPreference.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.lineage.icon;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.ImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.launcher3.R;
+import com.android.launcher3.lineage.icon.providers.CustomIconProvider;
+import com.android.launcher3.lineage.settings.RadioHeaderPreference;
+
+
+public class IconPackHeaderPreference extends RadioHeaderPreference {
+    private static final String TAG = "IconPackHeaderPreference";
+    private static final int PREVIEW_ICON_NUM = 4;
+    // This value has been selected as an average of usual "device profile-computed" values
+    private static final int PREVIEW_ICON_DPI = 500;
+
+    private final Context context;
+    private ImageView[] icons = null;
+
+    public IconPackHeaderPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+
+        setLayoutResource(R.layout.preference_widget_icons_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        final ImageView[] imageViews = {
+            (ImageView) holder.findViewById(R.id.pref_icon_a),
+            (ImageView) holder.findViewById(R.id.pref_icon_b),
+            (ImageView) holder.findViewById(R.id.pref_icon_c),
+            (ImageView) holder.findViewById(R.id.pref_icon_d)
+        };
+        this.icons = imageViews;
+        onRadioElementSelected(null);
+    }
+
+    @Override
+    public void onDetached() {
+        this.icons = null;
+        super.onDetached();
+    }
+
+    @Override
+    public void onRadioElementSelected(String key) {
+        if (icons == null) {
+            return;
+        }
+
+        final CustomIconProvider iconProvider = new CustomIconProvider(context);
+        final PackageManager pm = context.getPackageManager();
+        final LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
+        new GetLaunchableInfoTask(pm, launcherApps, PREVIEW_ICON_NUM, (aiList) -> {
+            for (int i = 0; i < icons.length; i++) {
+                icons[i].setImageDrawable(iconProvider.getIcon(
+                        aiList.get(i), PREVIEW_ICON_DPI, true));
+            }
+        }).execute();
+    }
+}

iconloaderlib/src/com/android/launcher3/lineage/icon/IconPackStore.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.lineage.icon;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import com.android.launcher3.icons.R;
+
+public final class IconPackStore {
+    public static final String SYSTEM_ICON_PACK = "android";
+    public static final String KEY_ICON_PACK = "pref_iconPackPackage";
+
+    private Context context;
+    private SharedPreferences prefs;
+
+    public IconPackStore(Context context) {
+        this.context = context;
+        this.prefs = context.getSharedPreferences(
+                "com.android.launcher3.prefs", Context.MODE_PRIVATE);
+    }
+
+    public String getCurrent() {
+        return prefs.getString(KEY_ICON_PACK, getDefaultIconPack());
+    }
+
+    public void setCurrent(String pkgName) {
+        prefs.edit()
+            .putString(KEY_ICON_PACK, pkgName)
+            .apply();
+    }
+
+    public boolean isUsingSystemIcons() {
+        return SYSTEM_ICON_PACK.equals(getCurrent());
+    }
+
+    public String getCurrentLabel(String defaultLabel) {
+        final String pkgName = getCurrent();
+        if (SYSTEM_ICON_PACK.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
+    }
+
+    private String getDefaultIconPack() {
+        return context.getString(R.string.icon_pack_default_pkg);
+    }
+}

src/com/android/launcher3/lineage/settings/RadioPreference.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.lineage.settings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+
+import com.android.launcher3.R;
+
+public class RadioPreference extends CheckBoxPreference {
+
+    public RadioPreference(Context context) {
+        this(context, null);
+    }
+
+    public RadioPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public RadioPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setWidgetLayoutResource(R.layout.preference_widget_radiobutton);
+        setLayoutResource(R.layout.preference_radio);
+        setIconSpaceReserved(false);
+    }
+}

AOSPA/android_packages_apps_ParanoidLauncher

src/com/android/launcher3/aospa/icon/GetLaunchableInfoTask.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.aospa.icon;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.LauncherActivityInfo;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Process;
+import android.os.UserHandle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+final class GetLaunchableInfoTask extends AsyncTask<Void, Void, List<LauncherActivityInfo>> {
+
+    private final PackageManager pm;
+    private final LauncherApps launcherApps;
+    private final int limit;
+    private final Callback callback;
+
+    GetLaunchableInfoTask(PackageManager pm,
+                          LauncherApps launcherApps,
+                          int limit,
+                          Callback callback) {
+        this.pm = pm;
+        this.launcherApps = launcherApps;
+        this.limit = limit;
+        this.callback = callback;
+    }
+
+    @Override
+    protected List<LauncherActivityInfo> doInBackground(Void... voids) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            // This should never happen
+            return new ArrayList<>();
+        }
+
+        final UserHandle ua = Process.myUserHandle();
+        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null)
+                .addCategory(Intent.CATEGORY_LAUNCHER);
+        return pm.queryIntentActivities(mainIntent, 0)
+            .parallelStream()
+            .sorted(new ResolveInfo.DisplayNameComparator(pm))
+            .limit(limit)
+            .map((ri) -> {
+                final ActivityInfo ai = ri.activityInfo;
+                final Intent i = new Intent();
+                i.setClassName(ai.applicationInfo.packageName, ai.name);
+                return launcherApps.resolveActivity(i, ua);
+            })
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void onPostExecute(List<LauncherActivityInfo> list) {
+        callback.onLoadCompleted(list);
+    }
+
+    interface Callback {
+        void onLoadCompleted(List<LauncherActivityInfo> result);
+    }
+}

src/com/android/launcher3/aospa/icon/IconPackHeaderPreference.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.aospa.icon;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.ImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.launcher3.R;
+import com.android.launcher3.icons.IconProvider;
+import com.android.launcher3.aospa.settings.RadioHeaderPreference;
+
+
+public class IconPackHeaderPreference extends RadioHeaderPreference {
+    private static final String TAG = "IconPackHeaderPreference";
+    private static final int PREVIEW_ICON_NUM = 4;
+    // This value has been selected as an average of usual "device profile-computed" values
+    private static final int PREVIEW_ICON_DPI = 500;
+
+    private final Context context;
+    private ImageView[] icons = null;
+
+    public IconPackHeaderPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public IconPackHeaderPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+
+        setLayoutResource(R.layout.preference_widget_icons_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        final ImageView[] imageViews = {
+            (ImageView) holder.findViewById(R.id.pref_icon_a),
+            (ImageView) holder.findViewById(R.id.pref_icon_b),
+            (ImageView) holder.findViewById(R.id.pref_icon_c),
+            (ImageView) holder.findViewById(R.id.pref_icon_d)
+        };
+        this.icons = imageViews;
+        onRadioElementSelected(null);
+    }
+
+    @Override
+    public void onDetached() {
+        this.icons = null;
+        super.onDetached();
+    }
+
+    @Override
+    public void onRadioElementSelected(String key) {
+        if (icons == null) {
+            return;
+        }
+
+        final IconProvider iconProvider = new IconProvider(context);
+        final PackageManager pm = context.getPackageManager();
+        final LauncherApps launcherApps = context.getSystemService(LauncherApps.class);
+        new GetLaunchableInfoTask(pm, launcherApps, PREVIEW_ICON_NUM, (aiList) -> {
+            for (int i = 0; i < icons.length; i++) {
+                icons[i].setImageDrawable(iconProvider.getIcon(
+                        aiList.get(i), PREVIEW_ICON_DPI));
+            }
+        }).execute();
+    }
+}

src/com/android/launcher3/aospa/icon/IconPackStore.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.aospa.icon;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import com.android.launcher3.R;
+
+public final class IconPackStore {
+    public static final String SYSTEM_ICON_PACK = "android";
+    public static final String KEY_ICON_PACK = "pref_iconPackPackage";
+
+    private Context context;
+    private SharedPreferences prefs;
+
+    public IconPackStore(Context context) {
+        this.context = context;
+        this.prefs = context.getSharedPreferences(
+                "com.android.launcher3.prefs", Context.MODE_PRIVATE);
+    }
+
+    public String getCurrent() {
+        return prefs.getString(KEY_ICON_PACK, getDefaultIconPack());
+    }
+
+    public void setCurrent(String pkgName) {
+        prefs.edit()
+            .putString(KEY_ICON_PACK, pkgName)
+            .apply();
+    }
+
+    public boolean isUsingSystemIcons() {
+        return SYSTEM_ICON_PACK.equals(getCurrent());
+    }
+
+    public String getCurrentLabel(String defaultLabel) {
+        final String pkgName = getCurrent();
+        if (SYSTEM_ICON_PACK.equals(pkgName)) {
+            return defaultLabel;
+        }
+
+        final PackageManager pm = context.getPackageManager();
+        try {
+            final ApplicationInfo ai = pm.getApplicationInfo(pkgName, 0);
+            return (String) pm.getApplicationLabel(ai);
+        } catch (PackageManager.NameNotFoundException e) {
+            return defaultLabel;
+        }
+    }
+
+    private String getDefaultIconPack() {
+        return context.getString(R.string.icon_pack_default_pkg);
+    }
+}

src/com/android/launcher3/aospa/settings/RadioPreference.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2020 Shift GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.aospa.settings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.CheckBoxPreference;
+
+import com.android.launcher3.R;
+
+public class RadioPreference extends CheckBoxPreference {
+
+    public RadioPreference(Context context) {
+        this(context, null);
+    }
+
+    public RadioPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public RadioPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setWidgetLayoutResource(R.layout.preference_widget_radiobutton);
+        setLayoutResource(R.layout.preference_radio);
+        setIconSpaceReserved(false);
+    }
+}